645e89326a4ca4c0d78f764022dc5319c6a998be
clement meyer <meyclem@gmail.com>
2017-08-22 15:30:22 +0200

    routes modified + dashboard.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :favorites
+  resources :favorites, only: [:index, :create, :destroy]
   resources :messages, only: [:index, :show, :new, :create]
   resources :influencers
 
   devise_for :users
   root to: 'pages#home'
-  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
