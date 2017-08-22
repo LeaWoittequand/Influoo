0ea4d703e7b3034bc2de27982f374c41eb992b03
clement meyer <meyclem@gmail.com>
2017-08-22 15:56:06 +0200

    modif routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :messages, only: [:index, :show, :new, :create]
   resources :influencers
 
+  get "/dashboard" => "pages#dashboard"
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
