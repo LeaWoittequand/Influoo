d856583f37927bef6cec2805d3303846fb2670f8
clement meyer <meyclem@gmail.com>
2017-08-23 12:04:54 +0200

    changes in db schema / models creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :messages
   has_one :influencer
   has_many :favorites
+  has_many :messages
+  has_many :conversations, foreign_key: :sender_id
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
