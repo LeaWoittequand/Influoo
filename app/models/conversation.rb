5c299a97cb8375667d33c94d00fdcca6aa423dcc
clement meyer <meyclem@gmail.com>
2017-08-23 13:03:20 +0200

    bug fix

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,8 +1,7 @@
 class Conversation < ApplicationRecord
-  add_index :conversations, [:recipient_id, :sender_id], unique: true
   has_many :messages, dependent: :destroy
-  belongs_to :sender, foreign_key: :sender_id, class_name: User
-  belongs_to :recipient, foreign_key: :recipient_id, class_name: User
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User"
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
