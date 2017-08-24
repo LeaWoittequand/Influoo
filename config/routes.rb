e665fcb3ca0a552390e5e49a68bddeec35140802
LeaWoittequand <lea.wtd@gmail.com>
2017-08-24 13:39:51 +0200

    fix navbar thème

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :influencers
 
   get "/dashboard" => "pages#dashboard"
+  get "test" => "pages#test"
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
