2523b3729dff3d1ba561c0b171c06b37ae94238c
LeaWoittequand <lea.wtd@gmail.com>
2017-08-24 12:55:37 +0200

    adds link on show + start favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,6 +9,7 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    # @favorites = current_user.favorites.create(params[:user_id])
   end
 
   def edit
@@ -20,3 +21,21 @@ class FavoritesController < ApplicationController
   def destroy
   end
 end
+
+
+ # def favorite
+ #    type = params[:type]
+ #    if type == "favorite"
+ #      current_user.favorites << @recipe
+ #      redirect_to :back, notice: 'You favorited #{@recipe.name}'
+
+ #    elsif type == "unfavorite"
+ #      current_user.favorites.delete(@recipe)
+ #      redirect_to :back, notice: 'Unfavorited #{@recipe.name}'
+
+ #    else
+ #      # Type missing, nothing happens
+ #      redirect_to :back, notice: 'Nothing happened.'
+ #    end
+ #  end
+
