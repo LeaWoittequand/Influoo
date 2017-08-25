2a75f4bcffb19513d81431a131f7bbfd9f6f0d75
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-25 13:34:41 +0200

    add influencer to favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,11 +5,15 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    @influencer =
-    @favorite = Favorite.new()
+    influencer = Influencer.find(params[:id])
+    favorite = Favorite.new(influencer: influencer, user: current_user)
 
-    if @favorite.save
-      redirect_to influencer_path(@influencer)
+    authorize(favorite)
+    if favorite.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
     # @favorites = current_user.favorites.create(params[:user_id])
   end
 
@@ -21,6 +25,28 @@ private
   def favorites_params
     params.require(:favorites).permit(:user, :infuencer)
   end
+
+
+# def favorite
+#     type = params[:type]
+#     if type == "favorite"
+#       current_user.favorites << @influencer
+#       redirect_to favorite_path, notice: 'You favorited #{@room.listing_name}'
+
+#     elsif type == "unfavorite"
+#       current_user.favorites.delete(@influencer)
+#       redirect_to favorite_path, notice: 'Unfavorited #{@room.listing_name}'
+
+#     else
+#       # Type missing, nothing happens
+#       redirect_to favorite_path, notice: 'Nothing happened.'
+#     end
+# end
+
+# private
+# def set_room
+#   @favorite = Favorite.find(params[:id])
+# end
 
 end
 
